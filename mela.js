7a6affe3b929feaf3c58b24f977ec94b871980f0
Seena James <you@example.com>
2016-08-08 18:32:40 +0530

    mela

--- mela.js
+++ mela.js
@@ -1,16 +1,28 @@
 $(document).ready(function(){
-			var a = $("#ex1").attr('id');
+			$(".zoom").zoom();
 			$('.pic-dimension').click(function() {
 				var idimg = $(this).attr('id');
 				var srcimg = $(this).attr('src');
-				 
-				 console.log("a");
 				$(".zoom-window").attr('src',srcimg);
+				$(".zoomImg").attr('src' ,srcimg);  
 			});
 			$('.slider-img').click(function() {
 				var idpic = $(this).attr('id');
 				var srcpic = $(this).attr('src');
 				$(".zoom-window").attr('src' ,srcpic);
-				$("zoom-window").zoom();
+				$(".zoomImg").attr('src' ,srcpic);
 			});
-		});
+});
+var zoomimage = [{
+    "Image1": "heroImages/congoblue1.jpg",
+    "Image2": "heroImages/congoblue2"
+  }, {
+    "Image1": "heroImages/dunebowl.jpg",
+    "Image2": "heroImages/dunebowl1.jpg"
+  }, {
+    "Image1": "heroImages/nutbowl1.jpg",
+    "Image2": "heroImages/nutbowl2.jpg"
+  }, {
+    "Image1": "heroImages/zambiabowl1.jpg",
+    "Image2": "heroImages/zambiabowl2.jpg"
+  }];
