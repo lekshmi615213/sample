1bf5fc0762697bab8ac79e341fb02dd211a5aa83
Seena James <you@example.com>
2016-08-10 13:36:40 +0530

    mela

--- mela.js
+++ mela.js
@@ -1,28 +1,48 @@
+var dataset = [ 
+{ "id": "congo", 
+"image1": "heroImages/congodecorative.jpg",
+"image2": "heroImages/congoblue2.jpg",
+"image3":"heroImages/congoblue1.jpg"
+}, {
+  "id": "dune",
+  "image1": "heroImages/dunebowl.jpg" ,
+  "image2":"heroImages/dunebowl.jpg",
+  "image3":"heroImages/dunebowl1.jpg"
+}, {
+  "id": "nut",
+  "image1": "heroImages/nutbowl1.jpg",
+  "image2": "heroImages/nutbowl1.jpg",
+  "image3":"heroImages/nutbowl2.jpg"
+  }, { 
+  	"id": "zambia",
+  	 "image1": "heroImages/zambiabowl1.jpg" ,
+  	 "image2": "heroImages/zambiabowl1.jpg",
+  	 "image3":"heroImages/zambiabowl2.jpg"
+  } ] 
+
 $(document).ready(function(){
 			$(".zoom").zoom();
-			$('.pic-dimension').click(function() {
+			$('#pic-dimension1').click(function() {
 				var idimg = $(this).attr('id');
 				var srcimg = $(this).attr('src');
 				$(".zoom-window").attr('src',srcimg);
 				$(".zoomImg").attr('src' ,srcimg);  
 			});
-			$('.slider-img').click(function() {
+			$('#pic-dimension2').click(function() {
+				var idimg = $(this).attr('id');
+				var srcimg = $(this).attr('src');
+				$(".zoom-window").attr('src',srcimg);
+				$(".zoomImg").attr('src' ,srcimg);  
+			});
+			$('.item').click(function() {
 				var idpic = $(this).attr('id');
-				var srcpic = $(this).attr('src');
-				$(".zoom-window").attr('src' ,srcpic);
-				$(".zoomImg").attr('src' ,srcpic);
+				for(var i=0; i<dataset.length; i++) {
+					if(idpic==dataset[i].id) {
+						$(".zoom-window").attr('src',dataset[i].image1);
+						$(".zoomImg").attr('src',dataset[i].image1);
+						$("#pic-dimension1").attr('src',dataset[i].image2);	
+						$("#pic-dimension2").attr('src',dataset[i].image3);				
+					}
+				}
 			});
 });
-var zoomimage = [{
-    "Image1": "heroImages/congoblue1.jpg",
-    "Image2": "heroImages/congoblue2"
-  }, {
-    "Image1": "heroImages/dunebowl.jpg",
-    "Image2": "heroImages/dunebowl1.jpg"
-  }, {
-    "Image1": "heroImages/nutbowl1.jpg",
-    "Image2": "heroImages/nutbowl2.jpg"
-  }, {
-    "Image1": "heroImages/zambiabowl1.jpg",
-    "Image2": "heroImages/zambiabowl2.jpg"
-  }];
