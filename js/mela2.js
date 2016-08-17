d090e4825053e191576e80483eeb95972475b105
Seena James <you@example.com>
2016-08-17 10:25:51 +0530

    first

--- js/mela2.js
+++ js/mela2.js
@@ -1,56 +1,43 @@
-var dataset = [ 
-{ "id": "congo", 
-"image1": "heroImages/congodecorative.jpg",
-"image2": "heroImages/congoblue2.jpg",
-"image3":"heroImages/congoblue1.jpg"
-}, {
-  "id": "dune",
-  "image1": "heroImages/dunebowl.jpg" ,
-  "image2":"heroImages/dunebowl.jpg",
-  "image3":"heroImages/dunebowl1.jpg"
-}, {
-  "id": "nut",
-  "image1": "heroImages/nutbowl1.jpg",
-  "image2": "heroImages/nutbowl1.jpg",
-  "image3":"heroImages/nutbowl2.jpg"
-  }, { 
-  	"id": "zambia",
-  	 "image1": "heroImages/zambiabowl1.jpg" ,
-  	 "image2": "heroImages/zambiabowl1.jpg",
-  	 "image3":"heroImages/zambiabowl2.jpg"
-  } ] 
-
+var dataset = {
+  	'congo': {
+		"image1": "heroImages/congodecorative.jpg",
+		"image2": "heroImages/congoblue2.jpg",
+		"image3":"heroImages/congoblue1.jpg"
+	},
+  	'dune': {
+		"image1": "heroImages/dunebowl.jpg" ,
+		"image2":"heroImages/dunebowl.jpg",
+		"image3":"heroImages/dunebowl1.jpg"
+	}, 
+  	'nut': {
+	  	"image1": "heroImages/nutbowl1.jpg",
+	  	"image2": "heroImages/nutbowl1.jpg",
+	  	"image3":"heroImages/nutbowl2.jpg"
+	},
+  	'zambia': {
+  	 	"image1": "heroImages/zambiabowl1.jpg" ,
+  	 	"image2": "heroImages/zambiabowl1.jpg",
+  	 	"image3":"heroImages/zambiabowl2.jpg"
+    }
+};
 $(document).ready(function(){
 	$(".zoom").zoom();
-	$('#pic-dimension1').click(function() {
-		var idimg = $(this).attr('id');
-		var srcimg = $(this).attr('src');
-		$(".zoom-window").attr('src',srcimg);
-		$(".zoomImg").attr('src' ,srcimg);  
-	});
-	$('#pic-dimension2').click(function() {
-		var idimg = $(this).attr('id');
-		var srcimg = $(this).attr('src');
-		$(".zoom-window").attr('src',srcimg);
-		$(".zoomImg").attr('src' ,srcimg);  
-	});
+	$('.pic-dimension').on('click', function() {
+        var srcimg = $(this).attr('src');
+        $(".zoom-window").attr('src',srcimg);
+        $(".zoomImg").attr('src' ,srcimg);  
+    });
 	$('.item').click(function() {
 		var idpic = $(this).attr('id');
-		for(var i=0; i<dataset.length; i++) {
-			if(idpic==dataset[i].id) {
-				$(".zoom-window").attr('src',dataset[i].image1);
-				$(".zoomImg").attr('src',dataset[i].image1);
-				$("#pic-dimension1").attr('src',dataset[i].image2);	
-				$("#pic-dimension2").attr('src',dataset[i].image3);				
-			}
-		}
+			$(".zoom-window").attr('src',dataset[idpic].image1);
+			$(".zoomImg").attr('src',dataset[idpic].image1);
+			$("#pic-dimension1").attr('src',dataset[idpic].image2);	
+			$("#pic-dimension2").attr('src',dataset[idpic].image3);				
 	});
 	$("#button-submit").click(function(){
     	var t = $('.zoom-window').attr('src');
 		$("#bag-add").append("<div class='product-list'></div>");
 		$("#bag-add > div:last-child").append("<div class='product-select'>"+''+'<img class="bag-image" src="' + $('.zoom-window').attr('src') + '">'+"</div><br>");
-		var k = $('#item-title').text();
-		$(".product-select > div:last-child").append("<span class='product'>"+k+"</span>");
 	});
 	$(".sign-up").click(function(){
 		$("#bag-add").show();
