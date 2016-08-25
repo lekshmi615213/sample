5bb016443b7cfad4b0979b346b4ab579e035d205
Ajay krishnan <ajaykrishnan@qburst.com>
2016-08-25 18:17:46 +0530

    corrected mistakes

--- js/910.js
+++ js/910.js
@@ -2,28 +2,25 @@ var $item, $wHeight, $caruselHeight;
 function adjustHeight() {
   $item = $('.carousel .item');
   $wHeight = $(window).height();
-  $caruselHeight = 0.8*($wHeight);
+  $caruselHeight = 0.8 * ($wHeight);
   $item.height($caruselHeight); 
 }
 $( document ).ready(function() {
   adjustHeight();
   $item.eq(0).addClass('active');
-  $item.height($caruselHeight);
   $item.addClass('full-screen');
   $('.carousel img').each(function() {
     var $src = $(this).attr('src');
-    var $color = $(this).attr('data-color');
-    $(this).parent().css({
+      $(this).parent().css({
       'background-image': 'url(' + $src + ')',
-      'background-color': $color
     });
     $(this).remove();
   });
 });
 $(window).resize(function() {
   adjustHeight();
-})
+});
 $('.carousel').carousel({
-    interval: 3000,
-    pause: "false"
+  interval: 3000,
+  pause: "false"
 });
