d1e8e858c1b239a2578bca5c9d4344d80670b89e
Ajay krishnan <ajaykrishnan@qburst.com>
2016-09-06 10:10:27 +0530

    modified

--- js/seemy.js
+++ js/seemy.js
@@ -1,75 +1,19 @@
-$(window).resize(function() {
-  var windowSize = $( window ).width();
-  if (windowSize < 770) {
-  	$("#navbar-container").removeClass("navbar-fixed-top");
-  }
-  else {
-  	$("#navbar-container").addClass("navbar-fixed-top");
-  }
-});
-
-$("#view-map").click(function() {
-   $( "#form-in-map" ).hide();
-   $("#view-form").css("display", "block");
-});
-
-$("#view-form").click(function() {
-   $( "#form-in-map" ).show();
-   $("#view-form").css("display", "none");
-});
-
-function initialize() {
-  var map, infowindow, marker, i, elem, content;
-  var locations = [
-    {
-      "place": "Technopark Phase III",
-      "lat": 8.5474,
-      "lng": 76.8807
-    },
-    {
-      "place": "Trivandrum International Airport",
-      "lat": 8.4834,
-      "lng": 76.9198
-    }
-  ];
-  var mapProp, image, mapCenter, service;
-  mapCenter = new google.maps.LatLng(8.5474,76.8807);
-  mapProp = {
-    center: mapCenter,
-    zoom: 13,
-    scrollwheel:false,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  map = new google.maps.Map(document.getElementById("google-map"),mapProp);
-  infowindow = new google.maps.InfoWindow();
-  for (i = 0; i < locations.length; i++) {  
-    marker = new google.maps.Marker({
-      position: new google.maps.LatLng(locations[i].lat, locations[i].lng),
-      map: map
-    });
-    
-    google.maps.event.addListener(marker, 'click', (function(marker, i) {
-      return function() {
-        infowindow.setContent(locations[i].place);
-        infowindow.open(map, marker);
-      }
-    })(marker, i));
-  }
-  google.maps.event.addListener(map, 'click', function(event) {
-    this.setOptions({
-      scrollwheel:true
-    });
-  });
-
-  google.maps.event.addListener(map, 'mouseout', function(event) {
-    this.setOptions({
-      scrollwheel:false
-    });  
-  });
-}
-google.maps.event.addDomListener(window, 'load', initialize);
-
 $(document).ready(function() {
+  $(window).resize(function() {
+    var windowSize = $( window ).width();
+    if (windowSize < 770) {
+      $("#navbar-container").removeClass("navbar-fixed-top");
+    }
+    else {
+      $("#navbar-container").addClass("navbar-fixed-top");
+    }
+  });
+
+  $(".view").click(function() {
+    $(this).parent().hide(),
+    $(this).parent().siblings('.hide-part').show();
+  });
+
   $('#fill-up').bootstrapValidator({
     fields: {
       customername: {
@@ -123,7 +67,65 @@ $(document).ready(function() {
     localStorage[$(this).attr('name')] = $(this).val();
   });
 
-  $('#fill-up').submit(function() {
+  $('#fill-up').click(function() {
+    var userName = $('#name-area').val(), 
+      userId = $('#email-area').val(), 
+      userMsg = $('#send-msg').val(),
+      arr = [];
+      arr.push({name:userName, email:userId, message:userMsg});
+    localStorage.setItem('customerInfo', JSON.stringify(arr));
+    console.log(arr);
     localStorage.clear();
   });
 });
+
+function initialize() {
+  var map, infowindow, marker, i, elem, content;
+  var locations = [
+    {
+      "place": "Technopark Phase III",
+      "lat": 8.5474,
+      "lng": 76.8807
+    },
+    {
+      "place": "Trivandrum International Airport",
+      "lat": 8.4834,
+      "lng": 76.9198
+    }
+  ];
+  var mapProp, image, mapCenter, service;
+  mapCenter = new google.maps.LatLng(8.5474,76.8807);
+  mapProp = {
+    center: mapCenter,
+    zoom: 13,
+    scrollwheel:false,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById("google-map"),mapProp);
+  infowindow = new google.maps.InfoWindow();
+  for (i = 0; i < locations.length; i++) {  
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(locations[i].lat, locations[i].lng),
+      map: map
+    });
+    
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+      return function() {
+        infowindow.setContent(locations[i].place);
+        infowindow.open(map, marker);
+      }
+    })(marker, i));
+  }
+  google.maps.event.addListener(map, 'click', function(event) {
+    this.setOptions({
+      scrollwheel:true
+    });
+  });
+
+  google.maps.event.addListener(map, 'mouseout', function(event) {
+    this.setOptions({
+      scrollwheel:false
+    });  
+  });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
